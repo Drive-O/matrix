aba20a5d87fc68f10601344e2c4bf9f62404ed39
Philipp <PhilSantay@gmail.com>
2017-01-30 21:02:05 +0100

    Setup Enviroment

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp"),
+watch = require("gulp-watch"),
+postcss = require("gulp-postcss"),
+imports = require("postcss-import"),
+vars = require("postcss-simple-vars"),
+mixins = require("postcss-mixins"),
+nesting = require("postcss-nesting"),
+prefix = require("autoprefixer"),
+browserSync = require("browser-sync");
+
+
+gulp.task("default", function(){
+	console.log("Default");
+})
+
+gulp.task("css", function(){
+	gulp.src("./app/assets/styles/styles.css")
+	.pipe(postcss([imports, nesting, prefix, vars, mixins]))
+	.on("error", function(errorStatus){
+		console.log("Error: " + errorStatus);
+		this.emit("end");
+	})
+	.pipe(gulp.dest("./app/temp/styles/"));
+})
+
+gulp.task("watch", function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	})
+
+	watch("./app/assets/styles/**/*.css", function(){
+		gulp.start("cssInjection");
+	});
+	watch("./app/index.html", function(){
+		browserSync.reload();
+	})
+})
+
+gulp.task("cssInjection", ["css"], function(){
+	gulp.src("./app/temp/styles/styles.css")
+	.pipe(browserSync.stream());
+})
